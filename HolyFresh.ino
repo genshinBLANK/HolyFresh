9511c44f76ce05da66f9d2ce1745c5bcdfbc3c78
genshinBLANK <93366890+genshinBLANK@users.noreply.github.com>
2024-06-01 11:38:16 +0300

    add ultrasound functionality
    Circuit Diagram: https://imgur.com/a/QIMey6f

--- HolyFresh.ino
+++ HolyFresh.ino
@@ -1,24 +1,39 @@
 #include <Servo.h>
 
+
 Servo myservo;
+  const int trigPin = 12;
+  const int echoPin = 11;
 
 void setup() {
-  myservo.attach(9); 
-  myservo.write(0); 
+myservo.attach(9); 
+
+myservo.write(0); 
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600); // Initialize serial communication
 }
 
 int sprayInterval = 1000; // interval between sprays
 unsigned long previousMillis = 0;
-const int sprayDuration = 500;
+const int sprayDuration = 1000;
 
 void loop() {
   unsigned long currentMillis = millis();
-  
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+int duration = pulseIn(echoPin, HIGH);
+double distance = duration*0.035/2;
+Serial.println(distance);
   // spray every 1 second
-  if (currentMillis - previousMillis >= sprayInterval) {
+  if ((currentMillis - previousMillis >= sprayInterval*10) && distance >= 20 || distance == 0) {
     previousMillis = currentMillis;
     spray();
   }
+}
 
 void spray() 
 {
